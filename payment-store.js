585f04c756a8b46fd6cf0124e300342f32fbe8e9
yzdmusic <118027024+yzdmusic@users.noreply.github.com>
2025-04-16 20:29:18 +0700

    Delete payment-store.js

--- payment-store.js
+++
@@ -1,6 +0,0 @@
-function sendMessage(product) {
-    var phoneNumber = '6281218141079';
-    var message = "Hello, saya tertarik dengan t-shirt; " + product + ". Mohon informasinya";
-    var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
-    window.open(url);
-  }
